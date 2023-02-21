39cc9d9d6ab9bf315b7d646bbb0b82f6e182606d
vothanhtinh <vothanhtinh147@gmail.com>
2023-02-21 16:26:04 +0700

    demo

---
+++ WEBPHONE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WEBPHONE.Startup))]
+namespace WEBPHONE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
